9ec4a86fc8182e4fd38fd41f55e74c004d0e0fbd
HuraKolia123 <kolyagura569@ukr.net>
2024-09-08 18:17:11 +0300

    1

--- src/pages/home/ui/HomePage/HomePage.tsx
+++ src/pages/home/ui/HomePage/HomePage.tsx
@@ -1,8 +1,25 @@
 // react
+import { Input } from "@/shared/ui/Input";
 import { FC } from "react";
 
 interface HomePageProps {}
 
 export const HomePage: FC<HomePageProps> = ({}) => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+      }}
+    >
+      <Input
+        placeholder="Enter a Email"
+        type="text"
+        label="Email"
+        helperText="Enter a valid email"
+      />
+    </div>
+  );
 };
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,31 +1,89 @@
 // react
-import { ChangeEvent, FC, HTMLInputTypeAttribute } from "react";
+import {
+  ChangeEvent,
+  CSSProperties,
+  FC,
+  HTMLInputTypeAttribute,
+  ReactNode,
+  useState,
+} from "react";
+// libs
+import clsx from "clsx";
 // styles
 import styles from "./Input.module.scss";
 
 interface InputProps {
-  value: string | number;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
+  value: string;
   type: HTMLInputTypeAttribute;
-  disabled?: boolean;
+
+  inputSize: "extrasmall" | "medium" | "large" | "extralarge";
+  maxWidth?: CSSProperties["maxWidth"];
+  label: string;
+  helperText: string;
 }
 
 export const Input: FC<InputProps> = ({
-  value,
   onChange,
   placeholder,
   type,
-  disabled,
+  value,
+  helperText,
+  label,
+  inputSize = "medium",
+  maxWidth = "323px",
 }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const onFocusedToggle = () => {
+    setIsFocused(!isFocused);
+  };
+
   return (
-    <input
-      disabled={disabled}
-      className={styles.Input}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      type={type}
-    />
+    <div className={styles.inputContent} style={{ maxWidth: maxWidth }}>
+      {label && (
+        <label
+          htmlFor="input"
+          className={clsx(styles.label, {
+            [styles.labelSmall]:
+              inputSize === "extrasmall" || inputSize === "medium",
+
+            [styles.labelLarge]:
+              inputSize === "large" || inputSize === "extralarge",
+          })}
+        >
+          {label}
+        </label>
+      )}
+      <div
+        className={clsx(styles.inputWrapper, {
+          [styles.inputWrapperExtraSmall]: inputSize === "extrasmall",
+          [styles.inputWrapperMedium]: inputSize === "medium",
+          [styles.inputWrapperLarge]: inputSize === "large",
+          [styles.inputWrapperExtraLarge]: inputSize === "extralarge",
+          [styles.inputFocused]: isFocused,
+        })}
+      >
+        <input
+          onFocus={onFocusedToggle}
+          onBlur={onFocusedToggle}
+          id="input"
+          className={clsx(styles.input, {
+            [styles.inputTextSmall]:
+              inputSize === "extrasmall" || inputSize === "medium",
+            [styles.inputTextLarge]:
+              inputSize === "large" || inputSize === "extralarge",
+          })}
+          onChange={onChange}
+          placeholder={placeholder}
+          type={type}
+          value={value}
+        />
+      </div>
+      {helperText && (
+        <div className={clsx(styles.helperText, {})}>{helperText}</div>
+      )}
+    </div>
   );
 };
